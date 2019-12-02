61ec4cfedb160c84ebc12dad482f2f910b67e192
Lene Julien <julien.lene@gmail.com>
2019-12-02 11:32:52 +0100

    [rajoute mass truc]

---
+++ iot-front/src/app/models/users.ts
@@ -0,0 +1,14 @@
+export class User {
+    name: string;
+    lastname: string;
+    email: string;   
+   constructor(
+       name: string,
+       lastname: string,
+       email: string,
+       ) { 
+       this.name = name;
+       this.lastname = lastname;
+       this.email = email;
+   }
+}
---
+++ iot-front/src/app/services/auth.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { EnvService } from './env.service';
+import { User } from '../models/users';
+import { error } from 'util';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class AuthService {
+  
+    isLoggedIn = false;
+    token: any;
+  
+    constructor(
+      private http: HttpClient,
+      private storage: NativeStorage,
+      private env: EnvService,
+    ) { }
+  
+  register(fName: string, lName: string, email: string, telNumber: number, pwd: string) {
+      return this.http.post(this.env.API_URL + 'inscription',
+        {name: fName, lastname: lName, mail: email, tel: telNumber, password: pwd})
+        .pipe(
+          tap(token => {
+            this.token = token;
+            this.isLoggedIn = true;
+            sessionStorage.setItem("user_id", this.token['id']);
+            sessionStorage.setItem("name", this.token['name']);
+            sessionStorage.setItem("lastname", this.token['lastname']);
+            sessionStorage.setItem("email", this.token['email']);
+            return token;
+          }),
+        );
+    }
+    
+    login(email: string, pwd: string) {
+        return this.http.post(this.env.API_URL + 'connexion',
+          {mail: email,  password: pwd})
+          .pipe(
+            tap(token => {
+              this.token = token;
+              this.isLoggedIn = true;
+    
+              sessionStorage.setItem("user_id", this.token['id']);
+              sessionStorage.setItem("name", this.token['name']);
+              sessionStorage.setItem("lastname", this.token['lastname']);
+              sessionStorage.setItem("email", this.token['email']);
+              return token;
+            }),
+          );
+        }
+
+    getUserId() {
+        return sessionStorage.getItem('user_id');
+        }
+    }
